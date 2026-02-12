4323123060137928463a7f1a554d4e4f4cc3c091
Holly9001 <johnandshelly621@gmail.com>
2026-02-12 17:51:24 -0500

    bruh

--- code/index.php
+++ code/index.php
@@ -281,7 +281,7 @@ for( $i = 0; $i < count($socials); $i++ )
 	echo( '<a href="'.$link.'">'.$name.'</a><br/>' );
 }
 
-/*
+/* 
 echo '							</p>
 							<p>
 							<strong>'. tl('Releases:') .'</strong><br />';
